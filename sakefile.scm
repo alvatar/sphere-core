b1e8d19be7ce0478f700917eaefeea28ddd9620d
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-04-28 19:52:44 +0200

    improved sakefile

--- sakefile.scm
+++ sakefile.scm
@@ -1,20 +1,24 @@
+(define lib-directory "lib/")
+(define lib-name "ffi")
+(define lib-suffix ".o1")
+
 (define-task init ()
   (make-directory (current-build-directory)))
 
 (define-task clean (init)
   (delete-file (current-build-directory))
-  (delete-file "lib/"))
+  (delete-file lib-directory))
 
 (define-task compile (init)
   (gambit-compile-file
    "module.scm"
-   output: (string-append (current-build-directory) "ffi.o1")))
+   output: (string-append (current-build-directory) lib-name lib-suffix)))
 
 (define-task install (compile)
-  (make-directory "lib")
-  (delete-file "lib/ffi.o1")
-  (copy-file (string-append (current-build-directory) "ffi.o1") "lib/ffi.o1"))
+  (make-directory lib-directory)
+  (delete-file (string-append lib-directory lib-name lib-suffix))
+  (copy-file (string-append (current-build-directory) lib-name lib-suffix)
+             (string-append lib-directory lib-name lib-suffix)))
 
-(define-task all (compile)
+(define-task all (compile install)
   '(compile and install))
-
