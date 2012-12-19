ee3929cada3395a455cef2278b9ae5f1535f73f7
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-12-19 17:16:08 +0100

    added some higher-order macros

--- src/syntax-macros.scm
+++ src/syntax-macros.scm
@@ -1,24 +1,28 @@
-;;; Macros for building complex hygienic macros
+;;!! Macros for building complex hygienic macros
 
 ;;! Syntax error
 (define-syntax syntax-error
   (syntax-rules ()
     ((_) (0))))
 
-;;! Check if given syntax element is an identifier
-;; (if-identifier x 'yes 'no) => yes
-;; (if-identifier 9 'yes 'no) => no
-;; By Al Petrofsky
-(define-syntax if-identifier
+;;! A symbol? predicate at the macro-expand time
+;;	symbol?? FORM KT KF
+;; FORM is an arbitrary form or datum
+;; expands in KT if FORM is a symbol (identifier), Otherwise, expands in KF
+;; (symbol?? x 'yes 'no) => yes
+;; (symbol?? 9 'yes 'no) => no
+;; By Oleg Kiselyov
+(define-syntax symbol??
   (syntax-rules ()
-    ((if-identifier (x . y) consequent alternate) alternate)
-    ((if-identifier #(x ...) consequent alternate) alternate)
-    ((if-identifier x consequent alternate)
+    ((symbol?? (x . y) kt kf) kf)	; It's a pair, not a symbol
+    ((symbol?? #(x ...) kt kf) kf)	; It's a vector, not a symbol
+    ((symbol?? maybe-symbol kt kf)
      (let-syntax
-         ((test (syntax-rules ()
-                  ((test x c a) c)
-                  ((test y c a) a))))
-       (test foo consequent alternate)))))
+         ((test
+	   (syntax-rules ()
+	     ((test maybe-symbol t f) t)
+	     ((test x t f) f))))
+       (test abracadabra kt kf)))))
 
 ;;! Check if two identifiers are two occurrences of the same identifier
 ;; For the macro id-eq??, two identifiers are equivalent if only if they
@@ -111,3 +115,149 @@
       (display "Trace: ") (write 'x) (newline)
       x))))
 
+;-------------------------------------------------------------------------------
+; CPS macros
+;-------------------------------------------------------------------------------
+
+;;! Identity macro
+(define-syntax k!id
+  (syntax-rules ()
+    ((k!id x)
+     x)))
+
+;;! k!reverse ACC (FORM ...) K
+;; reverses the second argument, appends it to the first and passes the result to K
+;; (k!reverse () (1 2 () (4 5)) '!) ;==> '((4 5) () 2 1)
+;; (k!reverse (x) (1 2 () (4 5)) '!) ;==> '((4 5) () 2 1 x)
+;; (k!reverse (x) () '!) ;==> '(x)
+(define-syntax k!reverse
+  (syntax-rules (!)
+    ((k!reverse acc () (k-head ! . k-args))
+     (k-head acc . k-args))
+    ((k!reverse acc (x . rest) k)
+     (k!reverse (x . acc) rest k))))
+
+;-------------------------------------------------------------------------------
+; Macro-lambda
+;-------------------------------------------------------------------------------
+
+;;! Macro-lambda: A notation for a first-class parameterized future macro-expansion action
+
+;; The ??!lambda form is first class: it can be passed to and
+;; returned from R5RS macros, and can be nested.
+;; The ??!lambda form is interpreted by a macro ??!apply.
+;; To be more precise, we specify that the following macro-application
+;; (??!apply (??!lambda (bound-var ...) body) arg ...)
+;; expands to body, with all non-shadowed instances of ??! and bound-var
+;; hygienically replaced by arg.
+;; A ??!lambda form can have one or several bound variables; the
+;; corresponding ??!apply form should have just as many arguments.
+;;
+;; Examples:
+;; (??!apply (??!lambda (x) '(a . b)) foo)
+;; ;===evals-to===> '(a . b)
+;; (??!apply (??!lambda (x)
+;;   (list (??!apply (??!lambda (x) (list '(??! x) 5 '(??! x))) (1 2))
+;;        '(??! x))) (3 4))
+;; ;===expands-to===> (list (list '(1 2) 5 '(1 2)) '(3 4))
+;; ;===evals-to===> '(((1 2) 5 (1 2)) (3 4))
+;; (??!apply (??!lambda (x) (let (((??! x) 1)) (??! x))) foo)
+;; ;===expands-to===> ((lambda (foo) foo) 1)
+;; ;===evals-to===> 1
+;; (??!apply (??!lambda (x k) (??!apply (??! k) (+ 1 (??! x))))
+;;    4
+;;    (??!lambda (x) (display (??! x))))
+;; ;===evals-to===> 5
+;;
+;; Additional notes
+;; When a template contains an identifier that does not appear in the pattern,
+;; it is called "colored". For example,
+;; (define-syntax tid-1
+;;   (syntax-rules ()
+;;     ((_) (a))))
+;; (tid-1) ;;=> a
+;; (define-syntax tid-2
+;;   (syntax-rules ()
+;;     ((_ a) (a))))
+;; (tid-2 a) ;==> a
+;; (tid-2 tid-1)
+;; The most challenging was understanding of the difference
+;; between tid-3 and tid-31.
+;; (define-syntax tid-3
+;;   (syntax-rules ()
+;;     ((_ x)
+;;      (let-syntax
+;;        ((foo
+;;   (syntax-rules ()
+;;     ((_ y) (lambda (x) y)))))
+;;        (foo x)))))
+;; (tid-3 a) ; ==> (lambda (a~2~3) a)
+;; (define-syntax tid-31
+;;   (syntax-rules ()
+;;     ((_ x)
+;;      (let-syntax
+;;        ((foo
+;;   (syntax-rules ()
+;;     ((_ x y) (lambda (x) y)))))
+;;        (foo x x)))))
+;; (tid-31 a) ;==> (lambda (a~3) a~3)
+;; (define-syntax tid-4
+;;   (syntax-rules ()
+;;     ((_ x) (x))
+;;     ((_) (tid-4 a))))
+;; (tid-4 a) ;==> a
+;; By Oleg Kiselyov
+
+(define-syntax ??!apply
+  (syntax-rules (??!lambda)
+    ((_ (??!lambda (bound-var . other-bound-vars) body)
+        oval . other-ovals)
+     (letrec-syntax
+         ((subs
+           (syntax-rules (??! bound-var ??!lambda)
+             ((_ val k (??! bound-var))
+              (appl k val))
+                                        ; check if bound-var is shadowed in int-body
+             ((_ val k (??!lambda bvars int-body))
+              (subs-in-lambda val bvars (k bvars) int-body))
+             ((_ val k (x))   ; optimize single-elem list substitution
+              (subs val (recon-pair val k ()) x))
+             ((_ val k (x . y))
+              (subs val (subsed-cdr val k x) y))
+             ((_ val k x) ; x is an id other than bound-var, or number&c
+              (appl k x))))
+          (subsed-cdr       ; we've done the subs in the cdr of a pair
+           (syntax-rules () ; now do the subs in the car
+             ((_ val k x new-y)
+              (subs val (recon-pair val k new-y) x))))
+          (recon-pair   ; reconstruct the pair out of substituted comp
+           (syntax-rules ()
+             ((_ val k new-y new-x)
+              (appl k (new-x . new-y)))))
+          (subs-in-lambda          ; substitute inside the lambda form
+           (syntax-rules (bound-var)
+             ((_ val () kp  int-body)
+              (subs val (recon-l kp ()) int-body))
+                                        ; bound-var is shadowed in the int-body: no subs
+             ((_ val (bound-var . obvars) (k bvars) int-body)
+              (appl k (??!lambda bvars int-body)))
+             ((_ val (obvar . obvars) kp int-body)
+              (subs-in-lambda val obvars kp int-body))))
+          (recon-l      ; reconstruct lambda from the substituted body
+           (syntax-rules ()
+             ((_ (k bvars) () result)
+              (appl k (??!lambda bvars result)))))
+          (appl                       ; apply the continuation
+           (syntax-rules ()           ; add the result to the end of k
+             ((_ (a b c d) result)
+              (a b c d result))
+             ((_ (a b c) result)
+              (a b c result))))
+          (finish
+           (syntax-rules ()
+             ((_ () () exp)
+              exp)
+             ((_ rem-bvars rem-ovals exps)
+              (??!apply (??!lambda rem-bvars exps) . rem-ovals)))))
+       ;; In the following, finish is the continuation...
+       (subs oval (finish other-bound-vars other-ovals) body)))))
