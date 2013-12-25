ca209dc2b8d4da72d64c1e5a7f4174ec5f8a1db5
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-12-25 19:09:45 +0100

    moved macro utilities to prelude

--- src/spheres#.scm
+++ src/spheres#.scm
@@ -16,27 +16,9 @@
 (define ##current-expander 'alexpander)
 
 
-;-------------------------------------------------------------------------------
-; Macro utils
-;-------------------------------------------------------------------------------
+
 
 (include "prelude.scm")
-
-;;! symbol->keyword
-(define^ (symbol->keyword s)
-  (string->keyword (symbol->string s)))
-
-;;! keyword->symbol
-(define^ (keyword->symbol k)
-  (string->symbol (keyword->string k)))
-
-;;! Anything to symbol
-(define^ (->symbol o)
-  (string->symbol (object->string o)))
-
-;;! Anything to keyword
-(define^ (->keyword o)
-  (string->keyword (object->string o)))
 
 ;-------------------------------------------------------------------------------
 ; Sphere
