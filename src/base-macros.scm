4c2c0c44381cde7c453731995d1fdfcae6734d7d
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-01-21 20:12:15 +0100

    basic values stuff goes to base-macros

--- src/base-macros.scm
+++ src/base-macros.scm
@@ -803,6 +803,73 @@
 
 
 ;;------------------------------------------------------------------------------
+;;!! Multiple values
+
+;;! Values to list
+(define-syntax values->list
+  (syntax-rules ()
+    ((_ x)
+     (call-with-values (lambda () x) list))))
+
+;;! Values to vector
+(define-syntax values->vector
+  (syntax-rules ()
+    ((_ x)
+     (call-with-values (lambda () x) vector))))
+
+
+;;! Number of values produced
+(define-syntax values-length
+  (syntax-rules ()
+    ((_ producer)
+     (call-with-values
+         (lambda () producer)
+       (lambda v (length v))))))
+
+;;! Extract only the nth-value from a function returning multiple values
+(define-syntax values-ref
+  (syntax-rules ()
+    ((_ n producer)
+     (call-with-values
+         (lambda () producer)
+       (lambda v (list-ref v n))))))
+
+;;! All values pairs must satisfy the given 2-predicate
+(define-syntax pred2?+
+  (syntax-rules ()
+    ((_ ?pred ?a ?b)
+     (let ((la (values->list ?a))
+           (lb (values->list ?b)))
+       (let recur ((la la)
+                   (lb lb))
+         (cond
+          ((null? la) (if (null? lb) #t #f))
+          ((null? lb) (if (null? la) #t #f))
+          (else
+           (and (?pred (car la) (car lb))
+                (recur (cdr la)
+                       (cdr lb))))))))))
+
+;;! All values pairs must satisfy eq?
+(define-syntax eq?+
+  (syntax-rules ()
+    ((_ ?a ?b)
+     (pred2?+ eq? ?a ?b))))
+
+;;! All values pairs must satisfy eqv?
+(define-syntax eqv?+
+  (syntax-rules ()
+    ((_ ?a ?b)
+     (pred2?+ eqv? ?a ?b))))
+
+;;! All values pairs must satisfy equal?
+(define-syntax equal?+
+  (syntax-rules ()
+    ((_ ?a ?b)
+     (pred2?+ equal? ?a ?b))))
+
+
+;;------------------------------------------------------------------------------
 ;;!! TODO
 
 ;;; Define values allows sharing state between functions
