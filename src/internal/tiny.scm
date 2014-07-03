6b40775d2bc5bc774ec33d08e83f3e8eb3600d61
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-07-03 20:12:42 +0200

    added 3 basic string operations to tiny-lib

--- src/internal/tiny.scm
+++ src/internal/tiny.scm
@@ -73,3 +73,61 @@
       (append (quicksort (filter (lambda (x) (gt? (car l) x)) (cdr l)) gt?)
               (list (car l))
               (quicksort (filter (lambda (x) (not (gt? (car l) x))) (cdr l)) gt?))))
+
+;;! Split a string using a separator
+(define (string-split sep)
+  (lambda (str)
+    (call-with-input-string
+     str
+     (lambda (p)
+       (read-all p (lambda (p) (read-line p sep)))))))
+
+;; Concatenate strings
+(define (string-concatenate strings)
+  (define (%string-copy! to tstart from fstart fend)
+    (if (> fstart tstart)
+        (do ((i fstart (+ i 1))
+             (j tstart (+ j 1)))
+            ((>= i fend))
+          (string-set! to j (string-ref from i)))
+
+        (do ((i (- fend 1)                    (- i 1))
+             (j (+ -1 tstart (- fend fstart)) (- j 1)))
+            ((< i fstart))
+          (string-set! to j (string-ref from i)))))
+  (let* ((total (do ((strings strings (cdr strings))
+                     (i 0 (+ i (string-length (car strings)))))
+                    ((not (pair? strings)) i)))
+         (ans (make-string total)))
+    (let lp ((i 0) (strings strings))
+      (if (pair? strings)
+          (let* ((s (car strings))
+                 (slen (string-length s)))
+            (%string-copy! ans i s 0 slen)
+            (lp (+ i slen) (cdr strings)))))
+    ans))
+
+;; Join strings
+(define (string-join strings #!key (delim " ") (grammar 'infix))
+  (let ((buildit (lambda (lis final)
+                   (let recur ((lis lis))
+                     (if (pair? lis)
+                         (cons delim (cons (car lis) (recur (cdr lis))))
+                         final)))))
+    (cond ((pair? strings)
+           (string-concatenate
+            (case grammar
+              ((infix strict-infix)
+               (cons (car strings) (buildit (cdr strings) '())))
+              ((prefix) (buildit strings '()))
+              ((suffix)
+               (cons (car strings) (buildit (cdr strings) (list delim))))
+              (else (error "Illegal join grammar"
+                           grammar string-join)))))
+          ((not (null? strings))
+           (error "STRINGS parameter not list." strings string-join))
+          ((eq? grammar 'strict-infix)
+           (error "Empty list cannot be joined with STRICT-INFIX grammar."
+                  string-join))
+          (else ""))))
+
