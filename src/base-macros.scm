64f41df1c26b51f6da807774a25a6130ce093e25
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-08-08 21:14:06 +0200

    cond-expand-features

--- src/base-macros.scm
+++ src/base-macros.scm
@@ -56,8 +56,56 @@
 ;;------------------------------------------------------------------------------
 ;;!! SRFIs
 
+;;! SRFI-0: Feature-based conditional expansion construct
+;; .author Álvaro Castro-Castilla (Adapted to dynamically add new features)
+(define-macro add-cond-expand-feature!
+  (lambda (new-feature)
+    (let ((features (cons new-feature (##cond-expand-features))))
+      (##cond-expand-features features)
+      (for-each
+       eval
+       `((define-syntax syntax-rules-error
+           (syntax-rules ()
+             ((_) (0))))
+         (define-syntax cond-expand
+           (syntax-rules (and or not else ,@features)
+             ((cond-expand) (syntax-rules-error "Unfulfilled cond-expand"))
+             ((cond-expand (else body ...))
+              (begin body ...))
+             ((cond-expand ((and) body ...) more-clauses ...)
+              (begin body ...))
+             ((cond-expand ((and req1 req2 ...) body ...) more-clauses ...)
+              (cond-expand
+               (req1
+                (cond-expand
+                 ((and req2 ...) body ...)
+                 more-clauses ...))
+               more-clauses ...))
+             ((cond-expand ((or) body ...) more-clauses ...)
+              (cond-expand more-clauses ...))
+             ((cond-expand ((or req1 req2 ...) body ...) more-clauses ...)
+              (cond-expand
+               (req1
+                (begin body ...))
+               (else
+                (cond-expand
+                 ((or req2 ...) body ...)
+                 more-clauses ...))))
+             ((cond-expand ((not req) body ...) more-clauses ...)
+              (cond-expand
+               (req
+                (cond-expand more-clauses ...))
+               (else body ...)))
+             ,@(map
+                (lambda (cef)
+                  `((cond-expand (,cef body ...) more-clauses ...)
+                    (begin body ...)))
+                features)
+             ((cond-expand (feature-id body ...) more-clauses ...)
+              (cond-expand more-clauses ...)))))))))
+
 ;;! SRFI-2 AND-LET*: an AND with local bindings, a guarded LET* special form
-;; Ported to syntax-rules by Álvaro Castro-Castilla
+;; .author Álvaro Castro-Castilla (Ported to syntax-rules)
 (define-syntax and-let*
   (syntax-rules ()
     ((_ ())
